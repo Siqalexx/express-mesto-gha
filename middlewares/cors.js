d43ff10a73d64f1de2f807a1f247f014796ff68b
Alexander <TheEndyShow@yandex.ru>
2023-02-20 01:41:41 +0300

    fix: cors((((((((((((((

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -13,11 +13,12 @@ const cors = (req, res, next) => {
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Methods', requestHeaders);
+    res.header('Access-Control-Allow-Headers', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
   next();
