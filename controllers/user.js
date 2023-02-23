935ff971bce826b83cb17a3867634d69503cf882
Alexander <TheEndyShow@yandex.ru>
2023-02-24 01:13:21 +0300

    fead: add somesite

--- controllers/user.js
+++ controllers/user.js
@@ -125,6 +125,7 @@ const login = (req, res, next) => {
             .cookie('jwt', token, {
               maxAge: 3600000,
               httpOnly: true,
+              sameSite: 'None',
             })
             .send({ message: 'cookie is download' });
         })
